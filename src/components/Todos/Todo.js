00e726738ad0aa9937549eb9b4967903c3f022b2
Dzmitry Sashcheka <sheensheella@gmail.com>
2023-01-11 15:52:50 +0300

    Add an icon next to each task

--- src/components/Todos/Todo.js
+++ src/components/Todos/Todo.js
@@ -1,8 +1,10 @@
+import { RiTodoFill } from 'react-icons/ri'
 import styles from './Todo.module.css'
 
 function Todo({ todo, index, deleteTodo }) {
   return (
     <div className={styles.todo} onDoubleClick={() => deleteTodo(index)}>
+      <RiTodoFill className={styles.todoIcon} />
       <div className={styles.todoText}>{todo}</div>
     </div>
   )
